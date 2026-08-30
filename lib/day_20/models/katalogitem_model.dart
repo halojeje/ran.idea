a97a53bb13f69dc01f46941f43b21a67343b8895
Jeihan <jeihan.azib@gmail.com>
2026-08-30 22:23:57 +0700

    Modify KatalogItem model for nullable output
    Updated the KatalogItem model to include nullable output and adjusted the constructor accordingly.

--- lib/day_20/models/katalogitem_model.dart
+++ lib/day_20/models/katalogitem_model.dart
@@ -2,14 +2,16 @@ class KatalogItem {
   final String title;
   final String description;
   final String image;
-  final List<String> tags;
-  final String? output;
+  final List<String>
+  tags; // <--- Ditambahkan agar sesuai dengan data_katalogitem_2.dart
+  final String?
+  output; // <--- Dibuat opsional (nullable) jika masih ingin menyimpan data lama
 
   const KatalogItem({
     required this.title,
     required this.description,
     required this.image,
-    required this.tags,
+    required this.tags, // <--- Ditambahkan di konstruktor
     this.output,
   });
 }
